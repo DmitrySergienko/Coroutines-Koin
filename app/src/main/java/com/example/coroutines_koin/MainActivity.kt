11dc430c860d5eedb91f9ba5661144f914dbabd1
DmitrySergienko <dmitrisergienko@gmail.com>
2022-06-11 15:13:31 +0300

    2. Coroutine Scope and Dispatchers

--- app/src/main/java/com/example/coroutines_koin/MainActivity.kt
+++ app/src/main/java/com/example/coroutines_koin/MainActivity.kt
@@ -2,49 +2,41 @@ package com.example.coroutines_koin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.koin.android.ext.android.inject
-import org.koin.androidx.viewmodel.ext.android.viewModel
-import org.koin.core.parameter.parametersOf
-import org.koin.core.qualifier.named
-import java.lang.Exception
+import android.widget.Toast
+import kotlinx.coroutines.*
+
 
 class MainActivity : AppCompatActivity() {
+    //1. Создаем scope там где планируем его использоввать
+    //I0 - для пропостых лпераций, Main для UI, Uncofined -если нужно завершить операцию на томже потоке
 
-    //выводим(внедряем) нашу зависисимость
-    private val repo: Repo by inject(named("RepoB"))
-    private val someClass: SomeClass by inject()
-    private val classWithConstructor: SomeClassWithConstructor by inject { parametersOf("Here should be String!!") }
-    private val vm: MyViewModel by viewModel()
-    private val someClassContext: SomeClassContext by inject()
-
+    val scope = CoroutineScope(Dispatchers.IO)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-// 1) для получения объекта созданного единажды single
-        //repo.print()
 
-// 2) для получения зависимости (каждый раз новой через factory
-        // getKoin().get<RepoA>().print()
+        //создаем короутину
+        scope.launch {
+            doSomthing()
+            withContext(Dispatchers.Main){
+                Toast.makeText(this@MainActivity, "Toast!!!", Toast.LENGTH_SHORT).show()
+            }
 
-// 3) для получения зависисмости сборка комплесного объекта
-        // someClass.print()
+        }
 
-// 4) для получения зависисмоти параметризованного класса, лучше обернуть в try/catch
-        //так как параметр должен быть нужного типа
+    }
 
-        /* try {
-             classWithConstructor.emitData()
-         } catch (e: Exception) {
-             println("VVV $e")
-         }*/
+    suspend fun doSomthing(): String {
+        delay(2000)
 
-// 5)  для получения ViewModel
-        //vm.getData()
-
-// 6)  для получения context
-        someClassContext.print()
+        return "Вовращаем строку"
+    }
+    //завершаем все короутины
+    override fun onDestroy() {
+        scope.cancel()
+        super.onDestroy()
     }
 
 
