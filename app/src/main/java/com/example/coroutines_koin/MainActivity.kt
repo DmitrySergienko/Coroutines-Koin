96a31022296a7b585999962f314bff661768968e
DmitrySergienko <dmitrisergienko@gmail.com>
2022-06-12 07:26:11 +0300

    2. Coroutine Job

--- app/src/main/java/com/example/coroutines_koin/MainActivity.kt
+++ app/src/main/java/com/example/coroutines_koin/MainActivity.kt
@@ -3,27 +3,35 @@ package com.example.coroutines_koin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.coroutines_koin.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
 
 
 class MainActivity : AppCompatActivity() {
+
     //1. Создаем scope там где планируем его использоввать
     //I0 - для пропостых лпераций, Main для UI, Uncofined -если нужно завершить операцию на томже потоке
-
     val scope = CoroutineScope(Dispatchers.IO)
 
+    //создаем job для упорядочиния работы корутин
+    var job: Job? = null
+
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-
-        //создаем короутину
-        scope.launch {
-            doSomthing()
-            withContext(Dispatchers.Main){
-                Toast.makeText(this@MainActivity, "Toast!!!", Toast.LENGTH_SHORT).show()
+        binding.button.setOnClickListener {
+            //создаем короутину
+            job?.cancel()
+            job = scope.launch {
+                repeat(100) {
+                    delay(1000)
+                    println("VVV $it")
+                }
             }
-
         }
 
     }
@@ -33,6 +41,7 @@ class MainActivity : AppCompatActivity() {
 
         return "Вовращаем строку"
     }
+
     //завершаем все короутины
     override fun onDestroy() {
         scope.cancel()
